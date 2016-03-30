057c0566200e26791a86f909877ca7569855dd50
KrauseFx <KrauseFx@gmail.com>
2016-03-29 21:30:15 -0700

    Updated gem description

--- lib/lolcommits/version.rb
+++ lib/lolcommits/version.rb
@@ -1,5 +1,5 @@
 # -*- encoding : utf-8 -*-
 module Lolcommits
   VERSION = '0.6.4'.freeze
-  DESCRIPTION = 'A portal to your office'.freeze
+  DESCRIPTION = 'The most powerful way to share your commits'.freeze
 end
